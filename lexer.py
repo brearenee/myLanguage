d0fdd6f54f4a1de8729eb8622fce2914fe3ddb46
Brea <brearenee@gmail.com>
2023-09-21 13:38:44 -0600

    stubs from class

--- lexer.py
+++ lexer.py
@@ -107,9 +107,6 @@ class IllegalCharError(Error):
   def __init__(self, details):
     super().__init__('Illegal Character', details)
 
-'''
-
-
 class NumberNode ():
     def __init__(self, token):
         self.token = token
@@ -132,36 +129,59 @@ class OperationNode():
     ##customer representation method to print instring format.
 
 class Parser():
-    def __init__(tokens, tokenIndex):
-        tokens = self.tokens
-        tokenIndex = self.index
+    def __init__(self, tokens):   
+       ## self.position = 0
+        self.tokens = tokens
+        self.tokenIndex = 0
+        self.current_token = tokens[tokenIndex]
 
-    Tokens //class variables
-• Token index// class variables 
-• Advance method to read (return) tokens if inside the text length
-• factor method
-• term method
-• Expression method
-    def advance(): 
-        
-    def factor():
-        Get the current token from the lexer output
-• Check if its RR_INT or RR_FLOAT
-• If yes , advance to next token
-• and return the number node (class variable)
-
-  
-    def term():
-         Check if the left is a factor[ call factor method for this]
-• Check if operator token is RR_MUL or RR_DIV
-• Check if the right is a factor[ call factor method for this]
-• [ recurrent checking can be done using a loop- that is for your HW]
-• If yes , advance
-• and return the operation node (class variable)
-Expression method uses similar code with PLUS|MINUS operations
+        '''   Tokens //class variables
+        • Token index// class variables 
+    • Advance method to read (return) tokens if inside the text length '''
 
     def parse(self):
         result = self.expr()
         return result
-        
-        '''
+
+    def advance(self): 
+        self.tokenIndex += 1
+        if self.self_index < len(self.tokens):
+            self.current_token = self.tokens[self.token_index]
+        else:
+            self.current_token = None
+     
+    def factor(self):
+        token = self.current_token
+        if token.type in (BC_INT, BC_FLOAT):
+            advance()
+        return NumberNode(token)
+
+    '''    Get the current token from the lexer output
+    • Check if its RR_INT or RR_FLOAT
+    • If yes , advance to next token
+    • and return the number node (class variable)'''
+
+    def term(self):
+        left = factor()
+        while self.current_tok.type in (_____, _______):
+            op_token = self.current_token
+            __________  //move to next
+            right = _________
+            opTree = OperationNode(left, op_token, right)
+        return opTree
+        ''' Check if the left is a factor[ call factor method for this]
+    • Check if operator token is RR_MUL or RR_DIV
+    • Check if the right is a factor[ call factor method for this]
+    • [ recurrent checking can be done using a loop- that is for your HW]
+    • If yes , advance
+    • and return the operation node (class variable)
+    Expression method uses similar code with PLUS|MINUS operations'''
+
+  def expression(self):
+    left = term()
+    while self.current_tok.type in (_____, _______):
+      op_token = self.current_token
+      __________  //move to next
+      right = _________
+      opTree = OperationNode(left, op_token, right)
+    return opTree
